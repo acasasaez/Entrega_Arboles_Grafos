b48fea0b5be80b9d4d5355201e3139d0aa242d9c
acasasaez <acasasae@myuax.com>
2022-11-25 21:13:24 +0100

    Create Huffman.py

---
+++ Ejercicio_1/Huffman.py
@@ -0,0 +1,20 @@
+#Se nos pide crear un arbol Huffman a partir de la siguiente tabla de frecuencias que nos permita codificar y decodificar mensajes.
+#TABLA: 0.2 F: 0.17 1: 0.13 3: 0.21 0: 0.05 M: 0.09 t: 0.15
+# ARBOL HUFFMAN:
+#            1
+#           /  \
+#          /    \
+#       0.41     0.59
+#       /  \     /   \
+#  0.20 0.21    0.27   0.32
+#             /  \      /  \
+#          0.13 0.14   0.15 0.17  
+#                 /  \
+#              0.05  0.09 
+
+import os 
+# Creamos nuestra clase nodo 
+class Nodo ():
+   probabilidad = 0.0
+   simbolo = ""
+   codificacion = ""
