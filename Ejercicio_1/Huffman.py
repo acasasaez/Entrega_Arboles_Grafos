abb13ac338d820a41cb823be68f702ab067e589c
acasasaez <acasasae@myuax.com>
2022-11-26 17:02:33 +0100

    a veces estoy haciendo codigo y le doy al copilot para que me acabe de escribir cosas en plan nombres de variables y así y me las escribe en inglés y mete funciones de por medio que ns porque le doy al enter si quererer

--- Ejercicio_1/Huffman.py
+++ Ejercicio_1/Huffman.py
@@ -68,27 +68,18 @@ class ArbolHuffman():
         self.Nodos.append(nodo) #Añadimos el nodo a la lista de nodos
         #Realizamos la codificación vinaria de los nodos
         if prob1 >= prob2:
-            self.Nodos[min1].encoding = "0"
-            self.Nodos[min2].encoding = "1"
+            self.Nodos[min1].codificacion = "0"
+            self.Nodos[min2].codificacion = "1"
         else:
-            self.Nodos[min1].encoding = "1"
-            self.Nodos[min2].encoding = "0"
+            self.Nodos[min1].codificacion = "1"
+            self.Nodos[min2].codificacion = "0"
             print ("Nodo padre: ", nodo.probabilidad) 
             print ("Nodo hijo izquierdo: ", self.Nodos[min1].probabilidad)
             print ("Nodo hijo derecho: ", self.Nodos[min2].probabilidad)
         min1 = self.NododeMinimaProb()
         min2 = self.NododeMinimaProb()
     
-dic = {"A": 0.2, 
-        "F": 0.17,
-          "1": 0.13, 
-          "3": 0.21, 
-          "0": 0.05, 
-          "M": 0.09, 
-          "T": 0.15}
-arbol = ArbolHuffman(dic)
-arbol.inicializarNodo()
-arbol.CrearArbol()
+
 
     #Un Arbol Huffman es un arbol binario que nos permitirá  codificacición y decodificación de mensajes a partir tablas de frecuencias. 
     #Para poder crear nuestreo árbol el primer paso será crear nuestra clase nodo que será la base de nuestro árbol y que contendrán la información ( simbolo y probabilidad) de cada nodo.
@@ -111,3 +102,22 @@ arbol.CrearArbol()
         # # Hasta que no se terminen los nodos, será necesario crear nodos intermedios que nos permitan construir el árbol desde nuestras probabilidades hasta la raiz. probabilidad 1 
         ##Añadimos nuestro nodo as la lista de nodos y codificamos los nodos hijos
         #recalculamos los nodos de mínima probabilidad de nuestra lista de nodos 
+
+        #Una vez que tenemos nuestro árbol procedemos a crear el diccionario que guarde el código binario de nuestros caracteres 
+   def DiccBinario(self):
+        for i in range(0, len(self.Nodos)):
+            if self.Nodos[i].simbolo != "":
+                self.DiccionarioBinario[self.Nodos[i].simbolo] = self.Nodos[i].codificacion
+        return self.DiccionarioBinario  
+
+dic = {"A": 0.2, 
+        "F": 0.17,
+          "1": 0.13, 
+          "3": 0.21, 
+          "0": 0.05, 
+          "M": 0.09, 
+          "T": 0.15}
+arbol = ArbolHuffman(dic)
+arbol.inicializarNodo()
+arbol.CrearArbol()
+arbol.diccionarioCodificacion()
